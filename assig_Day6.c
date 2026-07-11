795a4b4848e42653e5e4a4e1ca7aba3bc9f62aaa
Malak-Mohammed-Ahmed <malak.mohammed.esmail@gmail.com>
2026-07-11 15:11:44 +0300

    Add files via upload

---
+++ assig_Day6.c
@@ -0,0 +1,12 @@
+//Assigment create the Stdtype for following Types: 
+typedef unsigned char  U8_t ;
+typedef   signed char S8_T ;
+typedef unsigned short int U16_t ;  
+typedef   signed short int S16_t  ;
+typedef unsigned long int U32_t ;
+typedef  signed long int S32_t ;
+typedef unsigned long long int U64_t ;  
+typedef  signed long long int S64_t ;
+typedef float F32_t;
+typedef double F64_t ;
+typedef long double F96_t  ;
